568e4050fea1c6caba0971311468ec53d56584d4
Zach Howe <zach@zachhowe.com>
2023-11-17 17:42:06 -0800

    Initial Commit

---
+++ VideoLibrary/VideoLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoLibraryApp.swift
+//  VideoLibrary
+//
+//  Created by Zach Howe on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
